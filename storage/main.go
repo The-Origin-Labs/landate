ac82b479c315d16ec3ccf6466f070a1ff5dc6d81
siddhantprateek <siddhantprateek@gmail.com>
2023-06-26 12:23:01 +0530

    ✅ storage service discovery

--- storage/main.go
+++ storage/main.go
@@ -1,17 +1,37 @@
 package main
 
 import (
+	"log"
+	"strconv"
+
+	config "github.com/The-Origin-Labs/landate/config"
 	consul "github.com/The-Origin-Labs/landate/consul"
 
 	database "github.com/The-Origin-Labs/landate/storage/database"
 	api "github.com/The-Origin-Labs/landate/storage/routes"
 )
 
+const (
+	svcId   = "storage_svc_id"
+	svcName = "storage_svc"
+	envPORT = "STORAGE_SERVICE_PORT"
+	svcTag  = "storage"
+)
+
 func main() {
 	// Database connection
 	database.DBConnect()
-	consul.ServiceDicovery("storage-service")
+
+	svc_port, err := strconv.Atoi(config.GetEnvConfig(envPORT))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	storage_svc := consul.NewService(svcId, svcName, svcTag, svc_port)
+	// storage_svc.StartDicovery()
 
 	// api entrypoint
 	api.StorageService()
+	storage_svc.DeregisterService()
+
 }
